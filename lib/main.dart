0f46364fabee23fb35e5067e521c0182f5587062
Silvia Cárdenas <silvia2210102@correo.uis.edu.co>
2024-11-22 23:05:18 -0500

    Add tutorial and login

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  @override
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -110,8 +110,11 @@ class InitialScreen extends StatelessWidget {
           // Colectivo
           GestureDetector(
             onTap: () {
-              // Acción al presionar el botón
-              print('Colectivo tapped');
+              // Navegación a la pantalla de Tutorial
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TutorialScreen()),
+              );
             },
             child: Stack(
               alignment: Alignment.center,
@@ -150,3 +153,323 @@ class InitialScreen extends StatelessWidget {
     );
   }
 }
+
+// Nueva pantalla para el tutorial
+class TutorialScreen extends StatefulWidget {
+  @override
+  _TutorialScreenState createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  final List<Map<String, String>> tutorialData = [
+    {
+      "image": "assets/images/tuto1.png",
+      "title": "Como pasajero..",
+      "description":
+      "• Encontrarás las rutas ofrecidas por conductores.\n"
+          "• Puedes sugerir la creación de una ruta.\n"
+          "• Una vez reserves un cupo, recuerda confirmar antes del viaje.",
+    },
+    {
+      "image": "assets/images/tuto2.png",
+      "title": "Como conductor..",
+      "description":
+      "• Crea tu ruta con los puntos clave, precio y hora de salida.\n"
+          "• Recibe notificaciones de reservas y reasignaciones de cupos.\n"
+          "• Comunicación directa con chat grupal.",
+    },
+    {
+      "image": "assets/images/tuto3.png",
+      "title": "Tu opinión es importante",
+      "description":
+      "Califica a tu conductor o pasajeros al finalizar las rutas.\n"
+          "De este modo te ayudamos a escoger el servicio más adecuado para ti.",
+    },
+  ];
+
+  void _onSkip() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+  }
+
+  void _onNext() {
+    if (_currentPage < tutorialData.length - 1) {
+      _pageController.nextPage(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      _onSkip(); // Ir al LoginScreen al finalizar
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: TextButton(
+                onPressed: _onSkip,
+                child: Text(
+                  "SALTAR",
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (int index) {
+                  setState(() {
+                    _currentPage = index;
+                  });
+                },
+                itemCount: tutorialData.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          tutorialData[index]["image"]!,
+                          height: 250,
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          tutorialData[index]["title"]!,
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          tutorialData[index]["description"]!,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black54,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: _currentPage > 0
+                        ? () => _pageController.previousPage(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    )
+                        : null,
+                    child: Text(
+                      "ATRÁS",
+                      style: TextStyle(
+                        color:
+                        _currentPage > 0 ? Colors.green : Colors.grey[400],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: List.generate(
+                      tutorialData.length,
+                          (index) => Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentPage == index
+                              ? Colors.green
+                              : Colors.grey[300],
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: _onNext,
+                    child: Text(
+                      _currentPage < tutorialData.length - 1
+                          ? "SIGUIENTE"
+                          : "COMENZAR",
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton.icon(
+                onPressed: () {
+                  // Navegar al menú de inicio si es necesario
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.arrow_back, color: Colors.black),
+                label: Text(
+                  "Volver al menú de inicio",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Text(
+                  "Iniciar sesión",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("¿Primera vez? "),
+                    TextButton(
+                      onPressed: () {
+                        // Navegar a la página de registro
+                      },
+                      child: Text(
+                        "Regístrate",
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.green.shade50,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          prefixIcon: Icon(Icons.mail, color: Colors.black),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Contraseña",
+                          prefixIcon: Icon(Icons.lock, color: Colors.black),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {
+                            // Navegar a la página de "Olvidé mi contraseña"
+                          },
+                          child: Text(
+                            "Olvidé mi contraseña",
+                            style: TextStyle(color: Colors.green),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Acción al pulsar "Ingresar"
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                  ),
+                  child: Text(
+                    "Ingresar",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: IconButton(
+                  onPressed: () {
+                    // Implementar inicio de sesión con huella dactilar
+                  },
+                  icon: Icon(
+                    Icons.fingerprint,
+                    size: 50,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
